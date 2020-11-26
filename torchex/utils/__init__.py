e74320b1a22ef839c7c80f1dcead22b7d92b0275
vlue-c <hslee0390@gmail.com>
2020-11-26 15:58:17 +0900

    utils. area under curve

--- torchex/utils/__init__.py
+++ torchex/utils/__init__.py
@@ -2,3 +2,5 @@ from .onehot import logit_to_onehot, index_to_onehot
 from .normalization import min_max_normalization
 from .normalization import denormalize
 from .progress_bar import DummyPbar
+
+from .common import auc
